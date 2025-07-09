4f259e9aaed1a339b228afd451f68e2985ffcaa9
Muhammad-Hashir-55 <kingrocky742@gmail.com>
2025-07-09 14:03:02 +0500

    Initial upload of js-basics folder

---
+++ indexx.js
@@ -0,0 +1,43 @@
+// Comment anything using //
+
+// This is Beginner File for that person who is new To Programming Or New To JavaScript
+console.log('Hello World');
+let name = 'Hashir';
+console.log(name);
+
+
+
+const h = 12;
+console.log(h);
+
+let age = 30;
+console.log(typeof(age));
+let person = {name:'Mosh' , age:30};
+
+console.log(person);
+console.log(person.age)
+
+
+let selected = 'name'
+console.log(person[selected])
+
+
+let colours = ['red','blue'];
+colours[2] = 'green';
+colours[5] = 'purple';
+
+console.log(colours)
+console.log(colours[1])
+
+console.log(colours.length)
+
+
+
+// let us now create function
+function greet(name = 'unknown'){
+    console.log('Hello' +' '+ name);
+}
+greet('ahmed');
+
+
+
